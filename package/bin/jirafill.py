63636ea1abb12d3da8d0bc2f95e6f0c872962fdd
Guilhem Marchand <guilhem.marchand@gmail.com>
2021-07-24 13:28:33 +0100

    dev in progress

--- package/bin/jirafill.py
+++ package/bin/jirafill.py
@@ -55,30 +55,6 @@ class GenerateTextCommand(GeneratingCommand):
     def generate(self):
         storage_passwords = self.service.storage_passwords
 
-        # account configuration
-        isfound = False
-        jira_ssl_certificate_validation = None
-        jira_ssl_certificate_path = None
-        username = None
-        password = None
-
-        conf_file = "ta_service_desk_simple_addon_account"
-        confs = self.service.confs[str(conf_file)]
-        for stanza in confs:
-            if stanza.name == str(self.account):
-                isfound = True
-                for key, value in stanza.content.items():
-                    if key == "jira_url":
-                        jira_url = value
-                    if key == "jira_ssl_certificate_validation":
-                        jira_ssl_certificate_validation = value
-                    if key == "jira_ssl_certificate_path":
-                        jira_ssl_certificate_path = value
-                    if key == 'auth_type':
-                        auth_type = value
-                    if key == 'username':
-                        username = value
-
         # global configuration
         conf_file = "ta_service_desk_simple_addon_settings"
         confs = self.service.confs[str(conf_file)]
@@ -109,55 +85,154 @@ class GenerateTextCommand(GeneratingCommand):
               "https" : proxy_url + ":" + proxy_port
               }
 
-        # end of get configuration
+        # get all acounts
+        accounts = []
+        conf_file = "ta_service_desk_simple_addon_account"
+        confs = self.service.confs[str(conf_file)]
+        for stanza in confs:
+            # get all accounts
+            for name in stanza.name:
+                accounts.append(stanza.name)
+                break
 
-        # Stop here if we cannot find the submitted account
-        if not isfound:
-            self.logger.fatal('This acount has not been configured on this instance, cannot proceed!: %s', self)
-        # else get the password
+        # run
+        if self.account == '_all':
+
+            for account in accounts:            
+
+                # account configuration
+                jira_ssl_certificate_validation = None
+                jira_ssl_certificate_path = None
+                username = None
+                password = None
+
+                conf_file = "ta_service_desk_simple_addon_account"
+                confs = self.service.confs[str(conf_file)]
+                for stanza in confs:
+
+                    if stanza.name == str(account):
+                        for key, value in stanza.content.items():
+                            if key == "jira_url":
+                                jira_url = value
+                            if key == "jira_ssl_certificate_validation":
+                                jira_ssl_certificate_validation = value
+                            if key == "jira_ssl_certificate_path":
+                                jira_ssl_certificate_path = value
+                            if key == 'auth_type':
+                                auth_type = value
+                            if key == 'username':
+                                username = value
+
+                # end of get configuration
+
+                credential_username = str(account) + '``splunk_cred_sep``1'
+                credential_realm = '__REST_CREDENTIAL__#TA-jira-service-desk-simple-addon#configs/conf-ta_service_desk_simple_addon_account'
+                for credential in storage_passwords:
+                    if credential.content.get('username') == str(credential_username) \
+                        and credential.content.get('realm') == str(credential_realm) \
+                        and credential.content.get('clear_password').find('password') > 0:
+                        password = json.loads(credential.content.get('clear_password')).get('password')
+                        break
+
+                if jira_ssl_certificate_validation:
+                    if jira_ssl_certificate_validation == '0':
+                        ssl_verify = False
+                    elif jira_ssl_certificate_validation == '1' and jira_ssl_certificate_path and os.path.isfile(jira_ssl_certificate_path):
+                        ssl_verify = str(jira_ssl_certificate_path)
+                    elif jira_ssl_certificate_validation == '1':
+                        ssl_verify = True
+
+                if self.opt == 1:
+                    for project in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict ,'project'):
+                        usercreds = {'_time': time.time(), 'account': str(account), 'key':project.get('key'), 'key_projects':project.get('key')+" - "+project.get('name')}
+                        yield usercreds
+
+                if self.opt == 2:
+                    for issue in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'issuetype'):
+                        usercreds = {'_time': time.time(), 'account': str(account), 'issues':issue.get('name')}
+                        yield usercreds
+
+                if self.opt == 3:
+                    for priority in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'priority'):
+                        usercreds = {'_time': time.time(), 'account': str(account), 'priorities':priority.get('name')}
+                        yield usercreds
+
+                if self.opt == 4:
+                    for status in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'status'):
+                        result = {'_time': time.time(), 'account': str(account), 'status':status.get('name'), 'statusCategory':status.get('statusCategory').get('name')}
+                        yield result
+
         else:
-            credential_username = str(self.account) + '``splunk_cred_sep``1'
-            credential_realm = '__REST_CREDENTIAL__#TA-jira-service-desk-simple-addon#configs/conf-ta_service_desk_simple_addon_account'
-            for credential in storage_passwords:
-                if credential.content.get('username') == str(credential_username) \
-                    and credential.content.get('realm') == str(credential_realm) \
-                    and credential.content.get('clear_password').find('password') > 0:
-                    password = json.loads(credential.content.get('clear_password')).get('password')
-                    break
 
-        if jira_ssl_certificate_validation:
-            if jira_ssl_certificate_validation == '0':
-                ssl_verify = False
-            elif jira_ssl_certificate_validation == '1' and jira_ssl_certificate_path and os.path.isfile(jira_ssl_certificate_path):
-                ssl_verify = str(jira_ssl_certificate_path)
-            elif jira_ssl_certificate_validation == '1':
-                ssl_verify = True
+            # account configuration
+            isfound = False
+            jira_ssl_certificate_validation = None
+            jira_ssl_certificate_path = None
+            username = None
+            password = None
 
-        # debug
-        self.logger.fatal('DEBUG!: %s', self)
-        self.logger.fatal(str(jira_url))
-        self.logger.fatal(str(username))
-        self.logger.fatal(str(password))
+            conf_file = "ta_service_desk_simple_addon_account"
+            confs = self.service.confs[str(conf_file)]
+            for stanza in confs:
 
+                if stanza.name == str(self.account):
+                    isfound = True
+                    for key, value in stanza.content.items():
+                        if key == "jira_url":
+                            jira_url = value
+                        if key == "jira_ssl_certificate_validation":
+                            jira_ssl_certificate_validation = value
+                        if key == "jira_ssl_certificate_path":
+                            jira_ssl_certificate_path = value
+                        if key == 'auth_type':
+                            auth_type = value
+                        if key == 'username':
+                            username = value
 
-        if self.opt == 1:
-            for project in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict ,'project'):
-                usercreds = {'_time': time.time(), 'key':project.get('key'), 'key_projects':project.get('key')+" - "+project.get('name')}
-                yield usercreds
+            # end of get configuration
 
-        if self.opt == 2:
-            for issue in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'issuetype'):
-                usercreds = {'_time': time.time(), 'issues':issue.get('name')}
-                yield usercreds
+            # Stop here if we cannot find the submitted account
+            if not isfound:
+                self.logger.fatal('This acount has not been configured on this instance, cannot proceed!: %s', self)
+                
+            # else get the password
+            else:
+                credential_username = str(self.account) + '``splunk_cred_sep``1'
+                credential_realm = '__REST_CREDENTIAL__#TA-jira-service-desk-simple-addon#configs/conf-ta_service_desk_simple_addon_account'
+                for credential in storage_passwords:
+                    if credential.content.get('username') == str(credential_username) \
+                        and credential.content.get('realm') == str(credential_realm) \
+                        and credential.content.get('clear_password').find('password') > 0:
+                        password = json.loads(credential.content.get('clear_password')).get('password')
+                        break
 
-        if self.opt == 3:
-            for priority in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'priority'):
-                usercreds = {'_time': time.time(), 'priorities':priority.get('name')}
-                yield usercreds
+            if jira_ssl_certificate_validation:
+                if jira_ssl_certificate_validation == '0':
+                    ssl_verify = False
+                elif jira_ssl_certificate_validation == '1' and jira_ssl_certificate_path and os.path.isfile(jira_ssl_certificate_path):
+                    ssl_verify = str(jira_ssl_certificate_path)
+                elif jira_ssl_certificate_validation == '1':
+                    ssl_verify = True
 
-        if self.opt == 4:
-            for status in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'status'):
-                result = {'_time': time.time(), 'status':status.get('name'), 'statusCategory':status.get('statusCategory').get('name')}
-                yield result
+            if self.opt == 1:
+                for project in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict ,'project'):
+                    usercreds = {'_time': time.time(), 'account': str(self.account), 'key':project.get('key'), 'key_projects':project.get('key')+" - "+project.get('name')}
+                    yield usercreds
+
+            if self.opt == 2:
+                for issue in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'issuetype'):
+                    usercreds = {'_time': time.time(), 'account': str(self.account), 'issues':issue.get('name')}
+                    yield usercreds
+
+            if self.opt == 3:
+                for priority in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'priority'):
+                    usercreds = {'_time': time.time(), 'account': str(self.account), 'priorities':priority.get('name')}
+                    yield usercreds
+
+            if self.opt == 4:
+                for status in self.get_jira_info(username, password, jira_url, ssl_verify, proxy_dict , 'status'):
+                    result = {'_time': time.time(), 'account': str(self.account), 'status':status.get('name'), 'statusCategory':status.get('statusCategory').get('name')}
+                    yield result
+
 
 dispatch(GenerateTextCommand, sys.argv, sys.stdin, sys.stdout, __name__)

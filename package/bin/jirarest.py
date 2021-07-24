c7bf2e47036540e1c18ee8e433347a0adcdf758e
Guilhem Marchand <guilhem.marchand@gmail.com>
2021-07-24 14:49:00 +0100

    dev in progress

--- package/bin/jirarest.py
+++ package/bin/jirarest.py
@@ -17,7 +17,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import ta_jira_service_desk_simple_addon_declare
+import import_declare_test
 from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option, validators
 
 import json
@@ -33,6 +33,11 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 @Configuration(distributed=False)
 class GenerateTextCommand(GeneratingCommand):
 
+    account = Option(
+        doc='''
+        **Syntax:** **account=****
+        **Description:** JIRA account to be used, if unspecified the first account configured will be taken into account.''',
+        require=False, default=None)
     method = Option(
         doc='''
         **Syntax:** **method=****
@@ -61,59 +66,111 @@ class GenerateTextCommand(GeneratingCommand):
         )
         return response.json()
 
-    def generate(self): 
+    def generate(self):
         storage_passwords = self.service.storage_passwords
-        conf_file = "ta_jira_service_desk_simple_addon_settings"
+
+        # global configuration
+        conf_file = "ta_service_desk_simple_addon_settings"
         confs = self.service.confs[str(conf_file)]
+        jira_passthrough_mode = None
         proxy_enabled = "0"
         proxy_url = None
         proxy_dict = None
-        ssl_verify = False
-        ssl_cert_path = None
         for stanza in confs:
-            if stanza.name == "additional_parameters":
+            if stanza.name == "advanced_configuration":
                 for key, value in stanza.content.items():
-                    if key == "jira_username":
-                        username = value
-                    if key == "jira_url":
-                        url = value
-                    if key == "jira_ssl_certificate_validation":
-                        jira_ssl_certificate_validation = value
-                    if key == "jira_ssl_certificate_path":
-                        ssl_cert_path = value
+                    if key == "jira_passthrough_mode":
+                        jira_passthrough_mode = value
             if stanza.name == "proxy":
-               for key, value in stanza.content.items():
-                  if key == "proxy_enabled":
-                      proxy_enabled = value
-                  if key == "proxy_port":
-                      proxy_port = value
-                  if key == "proxy_rdns":
-                      proxy_rdns = value
-                  if key == "proxy_type":
-                      proxy_type = value
-                  if key == "proxy_url":
-                      proxy_url = value
+                for key, value in stanza.content.items():
+                    if key == "proxy_enabled":
+                        proxy_enabled = value
+                    if key == "proxy_port":
+                        proxy_port = value
+                    if key == "proxy_rdns":
+                        proxy_rdns = value
+                    if key == "proxy_type":
+                        proxy_type = value
+                    if key == "proxy_url":
+                        proxy_url = value
         if proxy_enabled == "1":
            proxy_dict= {
               "http" : proxy_url + ":" + proxy_port,
               "https" : proxy_url + ":" + proxy_port
               }
+
+        # get all acounts
+        accounts = []
+        conf_file = "ta_service_desk_simple_addon_account"
+        confs = self.service.confs[str(conf_file)]
+        for stanza in confs:
+            # get all accounts
+            for name in stanza.name:
+                accounts.append(stanza.name)
+                break
+            
+        # define the account target
+        if not self.account or self.account == '_any':
+            account = str(accounts[0])
+        else:
+            account = str(self.account)
+
+        # account configuration
+        isfound = False
+        jira_ssl_certificate_validation = None
+        jira_ssl_certificate_path = None
+        username = None
+        password = None
+
+        conf_file = "ta_service_desk_simple_addon_account"
+        confs = self.service.confs[str(conf_file)]
+        for stanza in confs:
+
+            if stanza.name == str(account):
+                isfound = True
+                for key, value in stanza.content.items():
+                    if key == "jira_url":
+                        jira_url = value
+                    if key == "jira_ssl_certificate_validation":
+                        jira_ssl_certificate_validation = value
+                    if key == "jira_ssl_certificate_path":
+                        jira_ssl_certificate_path = value
+                    if key == 'auth_type':
+                        auth_type = value
+                    if key == 'username':
+                        username = value
+
+        # end of get configuration
+
+        # Stop here if we cannot find the submitted account
+        if not isfound:
+            self.logger.fatal('This acount has not been configured on this instance, cannot proceed!: %s', self)
+            
+        # else get the password
+        else:
+            credential_username = str(account) + '``splunk_cred_sep``1'
+            credential_realm = '__REST_CREDENTIAL__#TA-jira-service-desk-simple-addon#configs/conf-ta_service_desk_simple_addon_account'
+            for credential in storage_passwords:
+                if credential.content.get('username') == str(credential_username) \
+                    and credential.content.get('realm') == str(credential_realm) \
+                    and credential.content.get('clear_password').find('password') > 0:
+                    password = json.loads(credential.content.get('clear_password')).get('password')
+                    break
+
+        # verify the url
+        if not jira_url.startswith("https://"):
+            jira_url = "https://" + str(jira_url)
+
+        # handle SSL verification and bundle
         if jira_ssl_certificate_validation:
             if jira_ssl_certificate_validation == '0':
                 ssl_verify = False
-            elif jira_ssl_certificate_validation == '1' and ssl_cert_path and os.path.isfile(ssl_cert_path):
-                ssl_verify = str(ssl_cert_path)
+            elif jira_ssl_certificate_validation == '1' and jira_ssl_certificate_path and os.path.isfile(jira_ssl_certificate_path):
+                ssl_verify = str(jira_ssl_certificate_path)
             elif jira_ssl_certificate_validation == '1':
                 ssl_verify = True
 
-        if not url.startswith("https://"):
-            url = "https://" + str(url)
-
-        for credential in storage_passwords:
-            if credential.content.get('username') == "additional_parameters``splunk_cred_sep``1" and credential.content.get('clear_password').find('jira_password') > 0:
-                password = json.loads(credential.content.get('clear_password')).get('jira_password')
-                break
-
+        # verify the method
         if self.method:
             jira_method = self.method
         else:
@@ -130,14 +187,14 @@ class GenerateTextCommand(GeneratingCommand):
             headers = {'Content-type': 'application/json'}
             if jira_method == "GET":
                 jira_fields_response = requests.get(
-                    url=str(url) + '/' + str(self.target),
+                    url=str(jira_url) + '/' + str(self.target),
                     auth=(username, password),
                     verify=ssl_verify,
                     proxies=proxy_dict
                 )
             elif jira_method == "DELETE":
                 jira_fields_response = requests.delete(
-                    url=str(url) + '/' + str(self.target),
+                    url=str(jira_url) + '/' + str(self.target),
                     auth=(username, password),
                     verify=ssl_verify,
                     proxies=proxy_dict
@@ -145,7 +202,7 @@ class GenerateTextCommand(GeneratingCommand):
             elif jira_method == "POST":
                 jira_fields_response = requests.post(
                     headers=headers,
-                    url=str(url) + '/' + str(self.target),
+                    url=str(jira_url) + '/' + str(self.target),
                     data=json.dumps(body_dict).encode('utf-8'),
                     auth=(username, password),
                     verify=ssl_verify,
@@ -154,7 +211,7 @@ class GenerateTextCommand(GeneratingCommand):
             elif jira_method == "PUT":
                 jira_fields_response = requests.put(
                     headers=headers,
-                    url=str(url) + '/' + str(self.target),
+                    url=str(jira_url) + '/' + str(self.target),
                     data=json.dumps(body_dict).encode('utf-8'),
                     auth=(username, password),
                     verify=ssl_verify,
